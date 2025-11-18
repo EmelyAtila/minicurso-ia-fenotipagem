00086372f00a1fef48df4d3a209265522e052d20
EmelyAtila <emelyatila@gmail.com>
2025-11-18 18:58:09 -0300

    Inserção das funções para melhoramento de imagem

--- fenotipagem-plantas/src/image_processor.py
+++ fenotipagem-plantas/src/image_processor.py
@@ -23,11 +23,25 @@ class PlantPhenotyping:
         # Carrega a imagem. cv2.imread retorna um array NumPy.
         self.original_image = cv2.imread(image_path)
 
-    # Tratamento de erro: se a imagem não carregar, lança uma exceção.
-    if self.original_image is None:
-       raise ValueError(f"Não foi possivél processar a imagem: {image_path}")
+        # Tratamento de erro: se a imagem não carregar, lança uma exceção.
+        if self.original_image is None:
+            raise ValueError(f"Não foi possivél processar a imagem: {image_path}")
 
-    # Variáveis de instância (atributos) para armazenar o estado do processamento
-    self.processed_image = None
-    self.mask = None
-    self.features = {}
+        # Variáveis de instância (atributos) para armazenar o estado do processamento
+        self.processed_image = None
+        self.mask = None
+        self.features = {}
+    
+    def preprocess_image(self) -> np.ndarray:
+        """
+        Pré-processa a imagem para melhorar a segmentação (redução de ruído).
+        """
+        #1. Converte de BGR (Padrão OpenCV) para RGB
+        img_rgb = cv2.cvtColor(self.original_image, cv2.COLOR_BGR2RGB)
+
+        #2. Aplicar Desfoque Gaussiano(Gaussian Blur) para reduzir ruído
+        #(5,5) é o tamanho do kernel, 0 é o desvio padrão 
+        blurred = cv2.GaussianBlur(img_rgb, (5, 5), 0)
+        
+        self.preprocess_image = blurred
+        return blurred
